e301883aaa2d628d723bde7f1668fa987f8d9cd4
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-11-30 16:30:02 +0530

    Update 268. Missing Number.java

--- 268. Missing Number.java
+++ 268. Missing Number.java
@@ -1,11 +1,32 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int actualSum = (n*(n+1))/2;
-        int exSum = 0;
-        for(int i:nums){
-            exSum+=i;
+    // public int missingNumber(int[] nums) {
+    //     int n = nums.length;
+    //     int actualSum = (n*(n+1))/2;
+    //     int exSum = 0;
+    //     for(int i:nums){
+    //         exSum+=i;
+    //     }
+    //     return actualSum-exSum;
+    // }
+    public int missingNumber(int[] nums){
+        int i = 0;
+        while(i<nums.length){
+            int c = nums[i];
+            if(c==nums.length){
+                i++;
+                continue;
+            }
+            if(nums[i]!=nums[c]){
+                int temp = nums[i];
+                nums[i] = nums[c];
+                nums[c] = temp;
+            }else{
+                i++;
+            }
         }
-        return actualSum-exSum;
+        for(i = 0; i<nums.length;i++){
+            if(nums[i]!= i) return i;
+        }
+        return nums.length;
     }
 }
