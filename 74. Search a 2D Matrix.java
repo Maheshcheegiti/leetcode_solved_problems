80d2cfcb8d9c825b8b0fe86f7dea7be559d701dd
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-12-06 21:27:27 +0530

    Update 74. Search a 2D Matrix.java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -13,20 +13,35 @@ class Solution {
         }
         return false;
     }
+    // public boolean searchMatrix(int[][] matrix, int target) {
+    //     int r1 = 0;
+    //     int r2 = matrix.length-1;
+    //     while(r1<=r2){
+    //         int rm = (r1+r2)/2;
+    //         if(matrix[rm][0]>target){
+    //             if(binarySearch(matrix,rm,target)) return true;
+    //             r2 = rm - 1;
+    //         }else if(matrix[rm][0]==target) return true;
+    //         else{
+    //             if(binarySearch(matrix,rm,target)) return true;
+    //             r1 = rm+1;
+    //         }
+    //     }
+    //     return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
         int r1 = 0;
         int r2 = matrix.length-1;
         while(r1<=r2){
             int rm = (r1+r2)/2;
             if(matrix[rm][0]>target){
-                if(binarySearch(matrix,rm,target)) return true;
-                r2 = rm - 1;
+                r2 = rm-1;
             }else if(matrix[rm][0]==target) return true;
             else{
-                if(binarySearch(matrix,rm,target)) return true;
                 r1 = rm+1;
             }
         }
+        if(r2>=0 && binarySearch(matrix,r2,target)) return true;
         return false;
     }
 }
