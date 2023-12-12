7f250cde692b4f387d4f46e0b2fcbd2b20eb45e3
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-12-12 15:37:59 +0530

    Update 69. Sqrt(x).java

--- 69. Sqrt(x).java
+++ 69. Sqrt(x).java
@@ -3,3 +3,21 @@ class Solution {
         return (int)Math.sqrt(x);
     }
 }
+// another solution
+class Solution {
+    public int mySqrt(int x) {
+        int l = 1;
+        int r = (int)Math.sqrt(x);
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(x / mid == mid){
+                return mid;
+            } else if(mid > x / mid){
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+}
