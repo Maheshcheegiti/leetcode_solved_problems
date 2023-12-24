66520bef66aaae5baa7db347763b2622715d977f
Cheegiti Mahesh <ramahesh024@gmail.com>
2023-12-24 09:05:57 +0530

    Create 1758. Minimum Changes To Make Alternating Binary String.java

---
+++ 1758. Minimum Changes To Make Alternating Binary String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minOperations(String s) {
+        int  c = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(i%2==0 && s.charAt(i)=='1') c++;
+            else if(i%2==1 && s.charAt(i)=='0') c++;
+        }
+        return Math.min(c, s.length()-c);
+    }
+}
