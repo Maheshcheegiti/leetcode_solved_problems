452027309e1b22b4757495c6780f19e36da11336
Cheegiti Mahesh <ramahesh024@gmail.com>
2024-02-05 23:09:16 +0530

    Create 1457. Pseudo-Palindromic Paths in a Binary Tree.java

---
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int result = 0;
+
+    public int pseudoPalindromicPaths(TreeNode root) {
+        solve(root, 0);
+        return result;
+    }
+
+    private void solve(TreeNode root, int temp) {
+        if (root != null) {
+            temp = temp ^ (1 << root.val);
+
+            if (root.left == null && root.right == null) {
+                if ((temp & (temp - 1)) == 0) {
+                    result++;
+                }
+            }
+
+            solve(root.left, temp);
+            solve(root.right, temp);
+        }
+    }
+}
