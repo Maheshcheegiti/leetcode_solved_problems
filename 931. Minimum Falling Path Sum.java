f92042ce016896ee659f19c39862b7c132571b12
Cheegiti Mahesh <ramahesh024@gmail.com>
2024-02-11 23:30:49 +0530

    Update 931. Minimum Falling Path Sum.java

--- 931. Minimum Falling Path Sum.java
+++ 931. Minimum Falling Path Sum.java
@@ -1,47 +1,32 @@
+class Solution {
+    int m;
+    int n;
+    Integer memo[][];
 
+    public int solve(int[][] matrix, int row, int col){
+        if(row>=m) return 0;
+        if(memo[row][col]!=null) return memo[row][col];
+        int sum = matrix[row][col];
 
-public class Solution {
-    public int minFallingPathSum(int[][] A) {
-        int m = A.length;
-        int n = A[0].length;
-
-        if (m == 1 || n == 1) return A[0][0];
-
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
-        
         int ans = Integer.MAX_VALUE;
-
-        for (int i = 0; i < A.length; ++i) {
-            ans = Math.min(ans, minFallingPathSum(A, 0, i, dp));
+        for(int i = -1; i<=1; i++){
+            int newCol = col+i;
+            if(newCol>=0 && newCol<n) ans = Math.min(ans, solve(matrix,row+1, newCol));
         }
 
-        return ans;
+        return memo[row][col] = sum+ans;
+
     }
 
-    private int minFallingPathSum(int[][] A, int row, int col, int[][] dp) {
-        int m = A.length;
-        int n = A[0].length;
-
-        if (dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
-
-        if (row == m - 1)
-            return dp[row][col] = A[row][col];
-
-        int left = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
-
-        if (col > 0)
-            left = minFallingPathSum(A, row + 1, col - 1, dp);
-
-        int straight = minFallingPathSum(A, row + 1, col, dp);
-
-        if (col < n - 1)
-            right = minFallingPathSum(A, row + 1, col + 1, dp);
-
-        dp[row][col] = Math.min(left, Math.min(straight, right)) + A[row][col];
-
-        return dp[row][col];
+    public int minFallingPathSum(int[][] matrix) {
+        int result = Integer.MAX_VALUE;
+        m = matrix.length;
+        n = matrix[0].length;
+        memo = new Integer[m][n];
+        int row  = 0;
+        for(int col = 0; col<n ; col++){
+            result = Math.min(result, solve(matrix, row, col ));
+        }
+        return result;
     }
 }
